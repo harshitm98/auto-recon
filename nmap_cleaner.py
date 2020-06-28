f589a9082f830e09e11647085c4d9fbe7209dbae
harshitm98 <harshitmah98@gmail.com>
2020-06-29 00:54:51 +0530

    Adds commandline argument -v for verbosity

--- nmap_cleaner.py
+++ nmap_cleaner.py
@@ -4,6 +4,9 @@ import xml.etree.ElementTree as ET
 import os
 import sys
 
+verbosity = False
+file_path = ""
+
 def extract_ip_and_ports(file_path):
 	tree = ET.parse(file_path)
 
@@ -19,11 +22,18 @@ def extract_ip_and_ports(file_path):
 		ip_ports[hostname] = ','.join(ports)
 
 	for ip, ports in ip_ports.items():
-		os.system("nmap -T4 -A -p{0} {1} -oN {1}-nmap.txt".format(ports, ip))
+		print("Running for ip : {} and ports : {}".format(ip, ports))
+		if verbosity:
+			os.system("nmap -T4 -A -p{0} {1} -oN {1}-nmap.txt".format(ports, ip))
+		else:
+			os.system("nmap -T4 -A -p{0} {1} -oN {1}-nmap.txt > /dev/null".format(ports, ip))
 
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		args = sys.argv[1:]
 		if '-i' in args:
 			file_path = args[args.index('-i') + 1]
+		if '-v' in args:
+			verbosity = True
+		if file_path != "":
 			extract_ip_and_ports(file_path)
