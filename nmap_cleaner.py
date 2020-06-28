6fa390b3810762f8ac252f4db1477a1838192999
harshitm98 <harshitmah98@gmail.com>
2020-06-29 00:49:08 +0530

    Takes commandline argument for nmap_cleaner.py

--- nmap_cleaner.py
+++ nmap_cleaner.py
@@ -2,20 +2,28 @@
 
 import xml.etree.ElementTree as ET
 import os
+import sys
 
-tree = ET.parse("Desktop/bugbounty/bpost/nmap_test.xml")
+def extract_ip_and_ports(file_path):
+	tree = ET.parse(file_path)
 
-root = tree.getroot()
+	root = tree.getroot()
 
-ip_ports = {}
+	ip_ports = {}
 
-for item in root.findall("./host"):
-	hostname = item.find('./hostnames/hostname').attrib['name']
-	print(hostname)
-	ports = []
-	for port in item.findall('./ports/port'):
-		ports.append(port.attrib['portid'])
-	ip_ports[hostname] = ','.join(ports)
+	for item in root.findall("./host"):
+		hostname = item.find('./hostnames/hostname').attrib['name']
+		ports = []
+		for port in item.findall('./ports/port'):
+			ports.append(port.attrib['portid'])
+		ip_ports[hostname] = ','.join(ports)
 
-for ip, ports in ip_ports.items():
-	print("nmap -T4 -A -p{0} {1} -oN {1}-nmap.txt".format(ports, ip))
+	for ip, ports in ip_ports.items():
+		os.system("nmap -T4 -A -p{0} {1} -oN {1}-nmap.txt".format(ports, ip))
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		args = sys.argv[1:]
+		if '-i' in args:
+			file_path = args[args.index('-i') + 1]
+			extract_ip_and_ports(file_path)
