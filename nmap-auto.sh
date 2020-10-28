d02c94f6a05b164eba47dcc860174806e594d06e
Harshit Maheshwari <fakebatman@localhost.localdomain>
2020-10-29 00:10:55 +0530

    Apply changes to compliment AutoRecon

--- nmap-auto.sh
+++ nmap-auto.sh
@@ -15,52 +15,44 @@ mkdir -p $PWD/nmap
 
 ## Nmap commands to be run
 
-# QUICK_SCAN="nmap -T4 $IP -oA nmap/quick-scan"
-# DEEP_QUICK_SCAN="nmap -T4 -A $IP -p$PORTS -oA nmap/deep-quick-scan"
-# VULN_QUICK_SCAN="nmap -T4 --script vuln $IP -p$PORTS -oA nmap/vuln-quick-scan"
+# SERVICE_SCAN="nmap -T4 -A $IP -p$PORTS -oA nmap/service-scan"
+# VULN_SCAN="nmap -T4 --script vuln $IP -p$PORTS -oA nmap/vuln-scan"
 # ALL_SCAN="nmap -T4 -p- $IP -oA nmap/all-scan"
 
-# Running QUICK_SCAN
+# Running ALL_SCAN
+echo ""
 echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # #"
-echo "#                     QUICK_SCAN                      #"
+echo "#                    ALL_SCAN_TCP                     #"
 echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # #"
 echo ""
-QUICK_SCAN="nmap -T4 $IP -oA nmap/quick-scan -Pn"
-eval $QUICK_SCAN
-
+ALL_SCAN="nmap -p- --min-rate 10000 -vvv $IP -oA nmap/all-scan -Pn"
+eval $ALL_SCAN
 
 # Extracting ports
 PORTS=""
-EXTRACTED_PORTS=$(grep -oP '\d{1,5}/open' nmap/quick-scan.gnmap)
+EXTRACTED_PORTS=$(grep -oP '\d{1,5}/open' nmap/all-scan.gnmap)
 for PORT in $(echo $EXTRACTED_PORTS);
 do  
     PORTS="${PORTS},${PORT::-5}"
 done
 PORTS="${PORTS:1}"
 
-# Running DEEP_QUICK_SCAN
+# Running SERVICE_SCAN
 echo ""
 echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # #"
-echo "#                   DEEP_QUICK_SCAN                   #"
+echo "#                    SERVICE_SCAN                     #"
 echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # #"
 echo ""
-DEEP_QUICK_SCAN="nmap -T4 -A $IP -p${PORTS} -oA nmap/deep-quick-scan -Pn"
-eval $DEEP_QUICK_SCAN
+SERVICE_SCAN="nmap -T4 -A $IP -p${PORTS} -oA nmap/service-scan -Pn"
+eval $SERVICE_SCAN
 
-# Running VULN_QUICK_SCAN
+# Running VULN_SCAN
 echo ""
 echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # #"
-echo "#                   VULN_QUICK_SCAN                   #"
+echo "#                      VULN_SCAN                      #"
 echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # #"
 echo ""
-VULN_QUICK_SCAN="nmap -T4 --script vuln $IP -p$PORTS -oA nmap/vuln-quick-scan -Pn"
-eval $VULN_QUICK_SCAN
+VULN_SCAN="nmap -T4 --script vuln $IP -p$PORTS -oA nmap/vuln-scan -Pn"
+eval $VULN_SCAN
 
-# Running ALL_SCAN
-echo ""
-echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # #"
-echo "#                      ALL_SCAN                       #"
-echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # #"
-echo ""
-ALL_SCAN="nmap -T4 -p- $IP -oA nmap/all-scan -Pn"
-eval $ALL_SCAN
+
